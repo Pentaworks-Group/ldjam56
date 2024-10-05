7c475d62966070fcf4e6c4e29ff0847f40b56dd0
Rentem <salasam@gmx.net>
2024-10-05 09:59:41 +0200

    more scene stuff

---
+++ Code/ldjam56/Assets/Scripts/Prefabs/GameFrame/Menus/MainMenuBaseBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using Assets.Scripts.Constants;
+
+using UnityEngine;
+
+namespace Assets.Scripts.Scenes.Menues
+{
+    public class MainMenuBaseBehaviour : BaseMenuBehaviour
+    {   
+        public void Play()
+        {
+            Base.Core.Game.PlayButtonSound();
+            Base.Core.Game.ChangeScene(SceneNames.Game);
+        }
+
+        public void ShowOptions()
+        {
+            Base.Core.Game.PlayButtonSound();
+            Base.Core.Game.ChangeScene(SceneNames.Options);
+        }
+
+        public void ShowCredits()
+        {
+            Base.Core.Game.PlayButtonSound();
+            Base.Core.Game.ChangeScene(SceneNames.Credits);
+        }
+    }
+}
