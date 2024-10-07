2d37e333e9cf394b8e08e2728d6fee1b4a0e65c5
XpArKeR <xparker@gmx.net>
2024-10-07 21:15:20 +0200

    removed testflag

--- Code/ldjam56/Assets/Scripts/Core/Definitons/GameMode.cs
+++ Code/ldjam56/Assets/Scripts/Core/Definitons/GameMode.cs
@@ -8,11 +8,7 @@ namespace Assets.Scripts.Core.Definitons
     public class GameMode : BaseDefinition
     {
         public String Name { get; set; }
-        public Boolean TestFlag { get; set; }
         public WorldDefinition World { get; set; }
-
-
-
 
         public List<BiomeDefinition> Biomes { get; set; }
         public List<EntityDefinition> Entities { get; set; }
