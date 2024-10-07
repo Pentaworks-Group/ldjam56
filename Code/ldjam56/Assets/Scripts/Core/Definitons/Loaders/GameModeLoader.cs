e64313b35c299cb71917bb0f5c288e112b77489f
Rentem <salasam@gmx.net>
2024-10-07 17:52:04 +0200

    more debugging

--- Code/ldjam56/Assets/Scripts/Core/Definitons/Loaders/GameModeLoader.cs
+++ Code/ldjam56/Assets/Scripts/Core/Definitons/Loaders/GameModeLoader.cs
@@ -43,8 +43,8 @@ namespace Assets.Scripts.Core.Definitons.Loaders
 
                         CheckItems(loadedGameMode.World.Biomes, newGameMode.World.Biomes, this.biomeCache);
                         CheckItems(loadedGameMode.World.Entities, newGameMode.World.Entities, this.entityCache);
-                        Debug.Log(newGameMode.Reference + " LoadedBiomes: " + newGameMode.World.Biomes.Count + " cache: " + biomeCache.Count + " checked: " + newGameMode.World.Biomes.Count);
-                        Debug.Log(newGameMode.Reference + " LoadedEntities: " + newGameMode.World.Entities.Count + " cache: " + entityCache.Count + " checked: " + newGameMode.World.Entities.Count);
+                        Debug.Log(newGameMode.Reference + " LoadedBiomes: " + loadedGameMode.World.Biomes.Count + " cache: " + biomeCache.Count + " checked: " + newGameMode.World.Biomes.Count);
+                        Debug.Log(newGameMode.Reference + " LoadedEntities: " + loadedGameMode.World.Entities.Count + " cache: " + entityCache.Count + " checked: " + newGameMode.World.Entities.Count);
                     }
 
                     targetCache[loadedGameMode.Reference] = newGameMode;
