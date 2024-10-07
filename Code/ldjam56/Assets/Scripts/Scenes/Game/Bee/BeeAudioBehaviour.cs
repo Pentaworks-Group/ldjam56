97a4ca05309304647f317886f12d3a04fd55cfc0
XpArKeR <xparker@gmx.net>
2024-10-07 13:33:51 +0200

    Bee Audio

--- Code/ldjam56/Assets/Scripts/Scenes/Game/Bee/BeeAudioBehaviour.cs
+++ Code/ldjam56/Assets/Scripts/Scenes/Game/Bee/BeeAudioBehaviour.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using UnityEngine;
 
@@ -10,6 +6,49 @@ namespace Assets.Scripts.Scenes.Game.Bee
 {
     public class BeeAudioBehaviour : MonoBehaviour
     {
+        private Boolean isStarted;
 
+        public Model.Bee bee;
+        public AudioSource beeAudioSource;
+
+        private void Start()
+        {
+            this.bee = Base.Core.Game.State?.Bee;
+
+            this.beeAudioSource.volume = GameFrame.Base.Audio.Ambience.Volume;
+            GameFrame.Base.Audio.Ambience.VolumeChanged.AddListener(OnAmbienceVolumeChanged);
+            Base.Core.Game.OnPauseToggled.AddListener(OnGamePauseToggled);
+        }
+
+        private void OnGamePauseToggled(Boolean isPaused)
+        {
+            if (isPaused)
+            {
+                this.beeAudioSource.Pause();
+            }
+            else
+            {
+                this.beeAudioSource.UnPause();
+            }
+        }
+
+        private void OnAmbienceVolumeChanged(Single volume)
+        {
+            this.beeAudioSource.volume = volume;
+        }
+
+        private void Update()
+        {
+            if (this.bee != null)
+            {
+                if (!isStarted)
+                {
+                    isStarted = true;
+
+                    this.beeAudioSource.volume = GameFrame.Base.Audio.Ambience.Volume;
+                    this.beeAudioSource.Play();
+                }
+            }
+        }
     }
 }
