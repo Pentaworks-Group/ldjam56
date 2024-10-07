4ddf4d8f578bcfccc6dfef51dc662c6ac6c8df22
XpArKeR <xparker@gmx.net>
2024-10-07 23:39:35 +0200

    Stuff

---
+++ Code/ldjam56/Assets/Scripts/Scenes/Credits/CameraRotationBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Scenes.Credits
+{
+    public class CameraRotationBehaviour : MonoBehaviour
+    {
+        public Transform target;
+        public float rotationSpeed = 10;
+
+        private void Update()
+        {
+            transform.LookAt(target);
+            transform.RotateAround(target.position, Vector3.up, -rotationSpeed * Time.deltaTime);
+        }
+    }
+}
