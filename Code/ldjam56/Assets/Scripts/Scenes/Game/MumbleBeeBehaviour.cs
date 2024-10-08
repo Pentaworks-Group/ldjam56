3f8ac9706393ab7c51d058bf6a1096653ce07dba
Rentem <salasam@gmx.net>
2024-10-08 18:05:08 +0200

    mumble bee rotate around center and spawn in circle

--- Code/ldjam56/Assets/Scripts/Scenes/Game/MumbleBeeBehaviour.cs
+++ Code/ldjam56/Assets/Scripts/Scenes/Game/MumbleBeeBehaviour.cs
@@ -5,9 +5,6 @@ namespace Assets.Scripts.Scenes.Game
     public class MumbleBeeBehaviour : MonoBehaviour
     {
         [SerializeField]
-        private GameObject homeSweetHome;
-
-        [SerializeField]
         private AudioSource audioSource;
 
         private Vector3 nextWaypoint;
@@ -19,6 +16,8 @@ namespace Assets.Scripts.Scenes.Game
         private bool isOut = false;
 
         private Vector3 rotationAxis = Vector3.up;
+
+        private Vector3 beeHiveCenter;
 
         private void Start()
         {
@@ -59,9 +58,9 @@ namespace Assets.Scripts.Scenes.Game
 
         public void Init(GameObject homeSweetHome)
         {
-            this.homeSweetHome = homeSweetHome;
-            var hP = homeSweetHome.transform.position;
-            transform.position = new Vector3(hP.x + 1, hP.y, hP.z + 1);
+            var renderer = homeSweetHome.GetComponentInChildren<Renderer>();
+            beeHiveCenter = renderer.bounds.center;
+            transform.position = beeHiveCenter;
 
             if (Random.value < 0.5f)
             {
@@ -75,7 +74,7 @@ namespace Assets.Scripts.Scenes.Game
 
         private void FlyAround()
         {
-            transform.RotateAround(homeSweetHome.transform.position, rotationAxis, rotationSpeed * Time.deltaTime);
+            transform.RotateAround(beeHiveCenter, rotationAxis, rotationSpeed * Time.deltaTime);
 
             var randVector = new Vector3(Random.Range(-.01f, .01f), Random.Range(-.01f, .01f), Random.Range(-.01f, .01f));
 
@@ -88,7 +87,7 @@ namespace Assets.Scripts.Scenes.Game
             if (dir.sqrMagnitude < distToWaypointReq)
             {
                 isOut = true;
-                transform.LookAt(homeSweetHome.transform);
+                transform.LookAt(beeHiveCenter);
                 transform.Rotate(rotationAxis * 90);
             }
             else
@@ -100,12 +99,13 @@ namespace Assets.Scripts.Scenes.Game
 
         private void ChooseNextWayPoint()
         {
-            var p = homeSweetHome.transform.position;
-            var d = new Vector3(Random.value, 0, Random.value);
+            var p = beeHiveCenter;
+            var d = new Vector3(Random.value -.5f, 0, Random.value -.5f);
             d.Normalize();
             d *= radius;
 
             this.nextWaypoint = new Vector3(p.x + d.x + Random.Range(-1f, 1f), p.y + Random.Range(-1f, 1f) + 4, p.z + d.z + Random.Range(-1f, 1f));
+            transform.LookAt(nextWaypoint);
         }
     }
 }
