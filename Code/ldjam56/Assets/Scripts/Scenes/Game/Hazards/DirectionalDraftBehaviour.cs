7bfa3bf7ecd3249918f6210efff99de25900d164
Rentem <salasam@gmx.net>
2024-10-18 18:40:20 +0200

    attempt to improve directionaldraft spawing

--- Code/ldjam56/Assets/Scripts/Scenes/Game/Hazards/DirectionalDraftBehaviour.cs
+++ Code/ldjam56/Assets/Scripts/Scenes/Game/Hazards/DirectionalDraftBehaviour.cs
@@ -27,10 +27,36 @@ namespace Assets.Scripts.Scenes.Game.Hazards
 
         public override void Init()
         {
-            var rand = Random.Range(0, 3);
-            transform.localScale += new Vector3(rand, rand, rand);
-            transform.rotation = Random.rotation;
-            gravity = transform.forward * strength;
+            //random scale
+            var scaleFactor = Random.Range(0f, 2f);
+            Debug.Log("scale: " + scaleFactor);
+            var scaleVector = new Vector3(scaleFactor, scaleFactor, scaleFactor);
+            transform.localScale += scaleVector;
+            transform.GetChild(0).localScale += scaleVector;
+
+            //random rotation, limit to max 30 degree down 
+            var xRotation = Random.Range(-210, 30);
+            var yRotation = Random.Range(0, 360);
+            var zRotation = Random.Range(0, 360);
+            transform.rotation = Quaternion.Euler(xRotation, yRotation, zRotation);
+
+            //set gravity
+            gravity = transform.forward * strength; 
+
+            //attempts do move forward/back
+            var before = transform.position;
+            var changeVector = transform.forward * scaleVector.x;
+            if (xRotation > 0 || xRotation < -180)
+            {
+                transform.position -= changeVector;
+            }
+            else
+            {
+                transform.position += changeVector;
+            }
+
+            Debug.LogFormat("forwad: {0}, factor: {1}, changeVector: {2}, scale: {3}", transform.forward, scaleFactor, changeVector, transform.localScale);
+            Debug.LogFormat("Before: {0}, after: {1}, xRotation: {2}", before, transform.position, xRotation);
         }
 
     }
